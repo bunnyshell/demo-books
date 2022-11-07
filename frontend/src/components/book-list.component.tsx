d96421a1bbf662f4ffb314f12a4c80fd432af627
Aris Buzachis <aris.buzachis@bunnyshell.com>
2022-11-07 17:46:37 +0200

    update
    Signed-off-by: Aris Buzachis <aris.buzachis@bunnyshell.com>

--- frontend/src/components/book-list.component.tsx
+++ frontend/src/components/book-list.component.tsx
@@ -143,7 +143,7 @@ export default class BookList extends Component<Props, State>{
         </div>
         ) : ( '' )}
         <div className="col-md-6">
-          <h4>Book List</h4>
+          <h4>Book List b</h4>
           </div>
         <div className="col-md-6">
           <div className="input-group mb-3">
