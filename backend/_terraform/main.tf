637e94a096f7d418dced2766aad281d0eb993bea
Sorin Dumitrescu <sorin.dumitrescu@bunnyshell.com>
2023-06-23 14:54:34 +0300

    Update Terraform module

--- backend/_terraform/main.tf
+++ backend/_terraform/main.tf
@@ -3,6 +3,10 @@ provider "aws" {}
 resource "aws_s3_bucket" "demo_bunnyshell_books" {
   bucket = var.bucket_name
   acl = "public-read"
+}
+
+resource "aws_s3_bucket_cors_configuration" "demo_bunnyshell_books_cors" {
+  bucket = aws_s3_bucket.demo_bunnyshell_books.bucket
 
   cors_rule {
     allowed_headers = ["*"]
