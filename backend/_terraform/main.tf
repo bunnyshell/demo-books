ae714c8f1484f12e3a44908f0e678d4e3626e4aa
Sorin Dumitrescu <sorin.dumitrescu@bunnyshell.com>
2023-06-23 16:12:33 +0300

    Fixed making the bucket public

--- backend/_terraform/main.tf
+++ backend/_terraform/main.tf
@@ -11,10 +11,23 @@ resource "aws_s3_bucket_ownership_controls" "demo_bunnyshell_books_ownership" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "demo_bunnyshell_books_public_access" {
+  bucket = aws_s3_bucket.demo_bunnyshell_books.bucket
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
 resource "aws_s3_bucket_acl" "demo_bunnyshell_books_acl" {
   bucket = aws_s3_bucket.demo_bunnyshell_books.bucket
   acl    = "public-read"
-  depends_on = [aws_s3_bucket_ownership_controls.demo_bunnyshell_books_ownership]
+
+  depends_on = [
+    aws_s3_bucket_ownership_controls.demo_bunnyshell_books_ownership,
+    aws_s3_bucket_public_access_block.demo_bunnyshell_books_public_access,
+  ]
 }
 
 resource "aws_s3_bucket_cors_configuration" "demo_bunnyshell_books_cors" {
