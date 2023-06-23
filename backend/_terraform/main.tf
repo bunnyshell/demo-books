11c191e4cd0d2b209b42c811a27419039e7110cb
Sorin Dumitrescu <sorin.dumitrescu@bunnyshell.com>
2023-06-23 14:59:54 +0300

    Update Terraform module to use separate ACL

--- backend/_terraform/main.tf
+++ backend/_terraform/main.tf
@@ -2,7 +2,16 @@ provider "aws" {}
 
 resource "aws_s3_bucket" "demo_bunnyshell_books" {
   bucket = var.bucket_name
-  acl = "public-read"
+}
+
+resource "aws_s3_bucket_acl" "demo_bunnyshell_books_acl" {
+  bucket = aws_s3_bucket.demo_bunnyshell_books.bucket
+
+  grant {
+    type        = "Group"
+    uri         = "http://acs.amazonaws.com/groups/global/AllUsers"
+    permissions = ["READ"]
+  }
 }
 
 resource "aws_s3_bucket_cors_configuration" "demo_bunnyshell_books_cors" {
