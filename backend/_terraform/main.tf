b4f631a8100ec811e54caeb2f0d18ce7e0a97ea3
Sorin Dumitrescu <sorin.dumitrescu@bunnyshell.com>
2023-06-23 15:04:07 +0300

    Updated Terraform module

--- backend/_terraform/main.tf
+++ backend/_terraform/main.tf
@@ -4,14 +4,21 @@ resource "aws_s3_bucket" "demo_bunnyshell_books" {
   bucket = var.bucket_name
 }
 
+resource "aws_s3_bucket" "demo_bunnyshell_books" {
+  bucket = var.bucket_name
+}
+
+resource "aws_s3_bucket_ownership_controls" "demo_bunnyshell_books_ownership" {
+  bucket = aws_s3_bucket.demo_bunnyshell_books.bucket
+  rule {
+    object_ownership = "ObjectWriter"
+  }
+}
+
 resource "aws_s3_bucket_acl" "demo_bunnyshell_books_acl" {
   bucket = aws_s3_bucket.demo_bunnyshell_books.bucket
-
-  grant {
-    type        = "Group"
-    uri         = "http://acs.amazonaws.com/groups/global/AllUsers"
-    permissions = ["READ"]
-  }
+  acl    = "public-read"
+  depends_on = [aws_s3_bucket_ownership_controls.demo_bunnyshell_books_ownership]
 }
 
 resource "aws_s3_bucket_cors_configuration" "demo_bunnyshell_books_cors" {
@@ -24,4 +31,6 @@ resource "aws_s3_bucket_cors_configuration" "demo_bunnyshell_books_cors" {
     expose_headers  = ["ETag"]
     max_age_seconds = 3000
   }
+
+  depends_on = [aws_s3_bucket.demo_bunnyshell_books]
 }
