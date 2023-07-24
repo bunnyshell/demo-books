475474c7f9a4878d50efb308f785db38d672e97a
Tudor Golubenco <tudor@xata.io>
2023-07-24 18:05:21 +0300

    Switch findAll to use Xata

--- backend/app/controllers/book.controller.js
+++ backend/app/controllers/book.controller.js
@@ -1,11 +1,24 @@
 const db = require("../models");
 const Book = db.books;
 const Op = db.Sequelize.Op;
+const { XataClient } = require("../models/xata");
+const fetch = require("node-fetch");
+
+let instance = undefined;
+const getXataClient = () => {
+  if (instance) return instance;
+
+  instance = new XataClient({
+    // Override configuration here
+    fetch: fetch,
+  });
+  return instance;
+};
 
 exports.create = (req, res) => {
   if (!req.body.title) {
     res.status(400).send({
-      message: "The title can not be empty"
+      message: "The title can not be empty",
     });
     return;
   }
@@ -13,33 +26,34 @@ exports.create = (req, res) => {
   const book = {
     title: req.body.title,
     description: req.body.description,
-    available: req.body.available ? req.body.available : false
+    available: req.body.available ? req.body.available : false,
   };
 
   Book.create(book)
-    .then(data => {
+    .then((data) => {
       res.send(data);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send({
-        message:
-          err.message || "An error occurred while adding the Book"
+        message: err.message || "An error occurred while adding the Book",
       });
     });
 };
 
 exports.findAll = (req, res) => {
   const title = req.query.title;
-  var condition = title ? { title: { [Op.iLike]: `%${title}%` } } : null;
+  var condition = title ? { title: { $contains: title } } : {};
 
-  Book.findAll({ where: condition })
-    .then(data => {
+  const xata = getXataClient();
+  xata.db.books
+    .filter(condition)
+    .getAll()
+    .then((data) => {
       res.send(data);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send({
-        message:
-          err.message || "An error occurred while retrieving the Books"
+        message: err.message || "An error occurred while retrieving the Books",
       });
     });
 };
@@ -48,18 +62,18 @@ exports.findOne = (req, res) => {
   const id = req.params.id;
 
   Book.findByPk(id)
-    .then(data => {
+    .then((data) => {
       if (data) {
         res.send(data);
       } else {
         res.status(404).send({
-          message: `Cannot find the Book with ID ${id}`
+          message: `Cannot find the Book with ID ${id}`,
         });
       }
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send({
-        message: `An error occurred while retrieving Book with ID ${id}`
+        message: `An error occurred while retrieving Book with ID ${id}`,
       });
     });
 };
@@ -68,22 +82,22 @@ exports.update = (req, res) => {
   const id = req.params.id;
 
   Book.update(req.body, {
-    where: { id: id }
+    where: { id: id },
   })
-    .then(num => {
+    .then((num) => {
       if (num == 1) {
         res.send({
-          message: "The Book was updated successfully"
+          message: "The Book was updated successfully",
         });
       } else {
         res.send({
-          message: `Cannot update the Book with ID ${id}`
+          message: `Cannot update the Book with ID ${id}`,
         });
       }
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send({
-        message: `An error occurred while updating Book with ID ${id}`
+        message: `An error occurred while updating Book with ID ${id}`,
       });
     });
 };
@@ -92,22 +106,22 @@ exports.delete = (req, res) => {
   const id = req.params.id;
 
   Book.destroy({
-    where: { id: id }
+    where: { id: id },
   })
-    .then(num => {
+    .then((num) => {
       if (num == 1) {
         res.send({
-          message: "The Book was deleted successfully"
+          message: "The Book was deleted successfully",
         });
       } else {
         res.send({
-          message: `Cannot delete the Book with ID ${id}`
+          message: `Cannot delete the Book with ID ${id}`,
         });
       }
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send({
-        message: `An error occurred while deleting the Book with ID ${id}`
+        message: `An error occurred while deleting the Book with ID ${id}`,
       });
     });
 };
@@ -115,28 +129,27 @@ exports.delete = (req, res) => {
 exports.deleteAll = (req, res) => {
   Book.destroy({
     where: {},
-    truncate: false
+    truncate: false,
   })
-    .then(nums => {
+    .then((nums) => {
       res.send({ message: `${nums} Books were deleted successfully` });
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send({
         message:
-          err.message || "An error occurred while removing all the Books"
+          err.message || "An error occurred while removing all the Books",
       });
     });
 };
 
 exports.findAllAvailable = (req, res) => {
   Book.findAll({ where: { available: true } })
-    .then(data => {
+    .then((data) => {
       res.send(data);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send({
-        message:
-          err.message || "An error occurred while retrieving the Books"
+        message: err.message || "An error occurred while retrieving the Books",
       });
     });
 };
